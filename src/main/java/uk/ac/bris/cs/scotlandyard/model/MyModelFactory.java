3d29710b199bcbef527e5b3d62af79b3565c3e19
olliemamuda <olliemamuda@gmail.com>
2022-03-23 11:04:51 +0000

    boilerplate for MyModel created.

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,7 +4,11 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
+import javafx.beans.Observable;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.ArrayList;
 
 /**
  * cw-model
@@ -17,5 +21,42 @@ public final class MyModelFactory implements Factory<Model> {
 	                                      ImmutableList<Player> detectives) {
 		// TODO
 		throw new RuntimeException("Implement me!");
+	}
+
+	private class MyModel implements Model{
+		ArrayList<Observer> observers;
+		Board.GameState gameState;
+
+		public MyModel(Board.GameState gameState){
+			observers = new ArrayList<>();
+			this.gameState = gameState;
+		}
+
+		@Nonnull
+		@Override
+		public Board getCurrentBoard() {
+			return null;
+		}
+
+		@Override
+		public void registerObserver(@Nonnull Observer observer) {
+			observers.add(observer);
+		}
+
+		@Override
+		public void unregisterObserver(@Nonnull Observer observer) {
+
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Observer> getObservers() {
+			return null;
+		}
+
+		@Override
+		public void chooseMove(@Nonnull Move move) {
+
+		}
 	}
 }
